fd320137db3797cffb61c5873ff58cad527a53e6
Wahyu Arief <wahyuief@gmail.com>
2019-07-21 20:10:17 +0700

    db

--- rekam_medis.sql
+++ rekam_medis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 30, 2019 at 06:42 AM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.0.32
+-- Generation Time: Jul 21, 2019 at 03:09 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,19 +32,26 @@ CREATE TABLE `audiometri` (
   `id` int(11) NOT NULL,
   `id_pasien` int(11) NOT NULL,
   `id_dokter` int(11) NOT NULL,
+  `hepar` text NOT NULL,
+  `empedu` text NOT NULL,
+  `pankreas` text NOT NULL,
+  `lien` text NOT NULL,
+  `ginjal` text NOT NULL,
+  `bulibuli` text NOT NULL,
+  `prostat` text NOT NULL,
+  `kesan` text NOT NULL,
   `keterangan` varchar(255) NOT NULL,
   `status` varchar(16) NOT NULL,
   `hapus` tinyint(1) NOT NULL,
-  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `tanggal` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `audiometri`
 --
 
-INSERT INTO `audiometri` (`id`, `id_pasien`, `id_dokter`, `keterangan`, `status`, `hapus`, `tanggal`) VALUES
-(6, 1, 1, '', 'Pending', 0, '2019-06-11 23:52:56'),
-(7, 2, 1, '', 'Pending', 0, '2019-06-11 23:53:01');
+INSERT INTO `audiometri` (`id`, `id_pasien`, `id_dokter`, `hepar`, `empedu`, `pankreas`, `lien`, `ginjal`, `bulibuli`, `prostat`, `kesan`, `keterangan`, `status`, `hapus`, `tanggal`) VALUES
+(1, 3, 2, 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'Selesai', 0, '2019-07-21 19:59:57');
 
 -- --------------------------------------------------------
 
@@ -65,7 +72,7 @@ CREATE TABLE `ekg` (
   `keterangan` varchar(255) NOT NULL,
   `status` varchar(16) NOT NULL,
   `hapus` tinyint(1) NOT NULL,
-  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `tanggal` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -73,7 +80,7 @@ CREATE TABLE `ekg` (
 --
 
 INSERT INTO `ekg` (`id`, `id_pasien`, `id_dokter`, `irama`, `rate`, `axis`, `kelainan`, `kesimpulan`, `saran`, `keterangan`, `status`, `hapus`, `tanggal`) VALUES
-(4, 2, 2, '1', '1', '1', '1', '1', '1', '1', 'Selesai', 0, '2019-06-11 23:15:28');
+(1, 3, 2, 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'Selesai', 0, '2019-07-21 19:59:57');
 
 -- --------------------------------------------------------
 
@@ -89,9 +96,9 @@ CREATE TABLE `obat` (
   `satuan` varchar(8) NOT NULL,
   `keterangan` varchar(255) NOT NULL,
   `tgl_masuk` date NOT NULL,
-  `hapus` tinyint(1) NOT NULL DEFAULT '0',
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `hapus` tinyint(1) NOT NULL DEFAULT 0,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -121,9 +128,9 @@ CREATE TABLE `pasien` (
   `alamat` varchar(255) NOT NULL,
   `pekerjaan` varchar(32) NOT NULL,
   `tgl_daftar` date NOT NULL,
-  `hapus` tinyint(1) NOT NULL DEFAULT '0',
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `hapus` tinyint(1) NOT NULL DEFAULT 0,
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -131,8 +138,8 @@ CREATE TABLE `pasien` (
 --
 
 INSERT INTO `pasien` (`id`, `no_pasien`, `ktp`, `nama`, `gender`, `tempat_lahir`, `tanggal_lahir`, `phone`, `goldarah`, `alamat`, `pekerjaan`, `tgl_daftar`, `hapus`, `updated_at`, `created_at`) VALUES
-(1, 'MHS0001', '3275071305980018', 'Wahyu Arief Trianto', 'L', 'Jakarta', '1998-05-13', '085219842984', 'O', 'Jl. Koperpu 31, Bekasi', 'Karyawan Swasta', '2019-04-07', 0, '2019-04-07 19:26:15', '2019-04-07 11:12:30'),
-(2, 'MHS0002', '3772131204982029', 'Yona Hergalina', 'P', 'Bandung', '1998-04-12', '089832482384', 'O', 'Jakarta', 'Mahasiswa', '2019-04-07', 0, '2019-04-07 19:08:42', '2019-04-07 19:08:42');
+(3, 'MHS0003', '327507123242432', 'Ujang Suaep', 'P', 'Bekasi', '1998-07-15', '08323434893', 'A', 'Dimana aja', 'Nganggur', '2019-07-13', 0, '2019-07-13 18:41:31', '2019-07-13 18:41:31'),
+(4, 'MHS0004', '3275071232423456', 'Sudirman', 'L', 'Rumah Sakit', '1992-06-07', '0812349248902', 'B', 'Boleh boleh', 'Bebas', '2019-06-21', 0, '2019-07-13 18:43:49', '2019-06-13 18:43:49');
 
 -- --------------------------------------------------------
 
@@ -152,18 +159,9 @@ CREATE TABLE `penanggung` (
   `perusahaan` varchar(32) NOT NULL,
   `hubungan` varchar(32) NOT NULL,
   `hapus` tinyint(1) NOT NULL,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `penanggung`
---
-
-INSERT INTO `penanggung` (`id`, `id_pasien`, `ktp`, `nama`, `gender`, `phone`, `alamat`, `pekerjaan`, `perusahaan`, `hubungan`, `hapus`, `updated_at`, `created_at`) VALUES
-(1, 1, '32750701234298193', 'Babeh', 'L', '088290200669', 'Komp. PU Sapta Taruna 4, Blok D No. 37', 'Karyawan Swasta', 'PT Wijaya Karya', 'Ayah', 0, '2019-05-01 08:41:00', '2019-04-14 11:53:39'),
-(3, 1, '3772131204982029', 'Yona Hergalina', 'P', '089832482384', 'Jakarta Timur', 'Mahasiswa', 'Unsada', 'Teman', 1, '2019-04-14 13:29:15', '2019-04-14 12:45:23'),
-(4, 2, '3275071305980019', 'Wahyu Arief Trianto', 'L', '085219842984', 'Bantargebang, Bekasi', 'Karyawan Swasta', 'PT Mitra Media Cakrawala', 'Teman', 1, '2019-04-14 13:24:39', '2019-04-14 12:56:31');
 
 -- --------------------------------------------------------
 
@@ -180,7 +178,7 @@ CREATE TABLE `rekammedis` (
   `diagnosa` varchar(32) NOT NULL,
   `tindakan` varchar(32) NOT NULL,
   `tanggal` date NOT NULL,
-  `hapus` tinyint(1) NOT NULL DEFAULT '0'
+  `hapus` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -188,8 +186,7 @@ CREATE TABLE `rekammedis` (
 --
 
 INSERT INTO `rekammedis` (`id`, `id_pasien`, `id_dokter`, `id_obat`, `keluhan`, `diagnosa`, `tindakan`, `tanggal`, `hapus`) VALUES
-(1, 2, 5, 2, 'Pusing Kepala Sebelah', 'Migrain', 'Istirahat', '2019-06-12', 0),
-(2, 1, 2, 1, 'Sakit Kepala', 'Pusing', 'Istirahat', '2019-06-23', 0);
+(1, 3, 2, 1, 'Sakit Kepala', 'Migrain', 'Tidur', '2019-07-21', 0);
 
 -- --------------------------------------------------------
 
@@ -211,7 +208,7 @@ CREATE TABLE `rontgen` (
   `keterangan` varchar(255) NOT NULL,
   `status` varchar(16) NOT NULL,
   `hapus` tinyint(1) NOT NULL,
-  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `tanggal` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -219,8 +216,7 @@ CREATE TABLE `rontgen` (
 --
 
 INSERT INTO `rontgen` (`id`, `id_pasien`, `id_dokter`, `cor`, `pulmo`, `costae`, `sinus`, `diapragma`, `jenis_periksa`, `kesan`, `keterangan`, `status`, `hapus`, `tanggal`) VALUES
-(10, 2, 2, '12', '32', '64', '445', '436', 'Test', 'good', 'AAA', 'Selesai', 0, '2019-06-11 22:00:44'),
-(11, 1, 1, '', '', '', '', '', '', '', '', 'Pending', 0, '2019-06-28 09:59:52');
+(1, 3, 2, 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'Selesai', 0, '2019-07-21 19:59:57');
 
 -- --------------------------------------------------------
 
@@ -240,7 +236,7 @@ CREATE TABLE `spirometri` (
   `keterangan` varchar(255) NOT NULL,
   `status` varchar(16) NOT NULL,
   `hapus` tinyint(1) NOT NULL,
-  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `tanggal` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -248,9 +244,7 @@ CREATE TABLE `spirometri` (
 --
 
 INSERT INTO `spirometri` (`id`, `id_pasien`, `id_dokter`, `nilai_prediksi`, `kvp`, `vep`, `ape`, `kesan`, `keterangan`, `status`, `hapus`, `tanggal`) VALUES
-(4, 1, 2, 1, 1, 1, 1, '1', 'test', 'Selesai', 0, '2019-06-11 21:57:29'),
-(5, 2, 1, 0, 0, 0, 0, '', '', 'Pending', 0, '2019-06-22 15:54:08'),
-(6, 1, 2, 23, 32, 54, 55, 'Bagus', 'Bagus', 'Selesai', 0, '2019-06-30 10:54:24');
+(1, 3, 2, 0, 0, 0, 0, 'test', 'test', 'Selesai', 0, '2019-07-21 19:59:57');
 
 -- --------------------------------------------------------
 
@@ -269,21 +263,22 @@ CREATE TABLE `users` (
   `spesialis` varchar(32) DEFAULT NULL,
   `gender` varchar(1) NOT NULL,
   `phone` varchar(15) NOT NULL,
-  `hapus` tinyint(1) NOT NULL DEFAULT '0',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `picture` varchar(255) DEFAULT NULL,
+  `hapus` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `nip`, `nama`, `alamat`, `jabatan`, `email`, `password`, `spesialis`, `gender`, `phone`, `hapus`, `created_at`, `updated_at`) VALUES
-(1, 31337, 'Wahyu Arief Trianto', 'Jl. Koperpu 31', 'Web Development', 'wahyu@sarjanaweb.com', '3b7375a688b1820b016224646365e127de125ff0', NULL, 'L', '085219842984', 0, '2019-04-04 21:10:03', '2019-04-07 12:13:20'),
-(2, 31338, 'Dr. Allya', 'Bekasi', 'Dokter', 'allya@gmail.com', 'c39bd3bdad05f1fedccb3864e58912adacc9f228', 'Gigi', 'P', '08324143829', 0, '2019-04-06 16:45:59', '2019-05-05 09:02:57'),
-(3, 31339, 'Surya Kencana', 'Bekasi', 'Pegawai', 'surya@gmail.com', '1d2727face9e8a5171fd434ca9ac155020be70db', NULL, 'L', '084234188332', 0, '2019-04-06 17:32:32', '2019-04-07 12:13:30'),
-(4, 31340, 'Diajeng Putri', 'Malang', 'Pegawai', 'putri@gmail.com', 'b9f08a0a16e7ed30ab463acaf6f15c46f9c5ff21', NULL, 'P', '081244359031', 0, '2019-04-06 17:35:11', '2019-04-11 20:07:56'),
-(5, 31341, 'Dr. Riana Dewi', 'Bandung', 'Dokter', 'riana@gmail.com', 'bf67983bf7400cac1a51913d276301b38581f309', 'Kulit', 'P', '08522138921', 0, '2019-04-06 17:41:17', '2019-04-06 17:41:17');
+INSERT INTO `users` (`id`, `nip`, `nama`, `alamat`, `jabatan`, `email`, `password`, `spesialis`, `gender`, `phone`, `picture`, `hapus`, `created_at`, `updated_at`) VALUES
+(1, 999999, 'Administrator', 'Jl. Koperpu 31', 'Web Development', 'wahyu@sarjanaweb.com', '3b7375a688b1820b016224646365e127de125ff0', NULL, 'L', '085219842984', NULL, 0, '2019-04-04 21:10:03', '2019-04-07 12:13:20'),
+(2, 31338, 'Dr. Allya', 'Bekasi', 'Dokter', 'allya@gmail.com', 'c39bd3bdad05f1fedccb3864e58912adacc9f228', 'Ahli Radiologi', 'P', '08324143829', NULL, 0, '2019-04-06 16:45:59', '2019-05-05 09:02:57'),
+(3, 31339, 'Surya Kencana', 'Bekasi', 'Pegawai', 'surya@gmail.com', '1d2727face9e8a5171fd434ca9ac155020be70db', NULL, 'L', '084234188332', NULL, 0, '2019-04-06 17:32:32', '2019-04-07 12:13:30'),
+(4, 31340, 'Diajeng Putri', 'Malang', 'Pegawai', 'putri@gmail.com', 'b9f08a0a16e7ed30ab463acaf6f15c46f9c5ff21', NULL, 'P', '081244359031', NULL, 0, '2019-04-06 17:35:11', '2019-04-11 20:07:56'),
+(5, 31341, 'Dr. Riana Dewi', 'Bandung', 'Dokter', 'riana@gmail.com', 'bf67983bf7400cac1a51913d276301b38581f309', 'Kulit', 'P', '08522138921', NULL, 0, '2019-04-06 17:41:17', '2019-04-06 17:41:17');
 
 --
 -- Indexes for dumped tables
@@ -363,13 +358,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `audiometri`
 --
 ALTER TABLE `audiometri`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `ekg`
 --
 ALTER TABLE `ekg`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `obat`
@@ -381,7 +376,7 @@ ALTER TABLE `obat`
 -- AUTO_INCREMENT for table `pasien`
 --
 ALTER TABLE `pasien`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `penanggung`
@@ -393,19 +388,19 @@ ALTER TABLE `penanggung`
 -- AUTO_INCREMENT for table `rekammedis`
 --
 ALTER TABLE `rekammedis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `rontgen`
 --
 ALTER TABLE `rontgen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `spirometri`
 --
 ALTER TABLE `spirometri`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
