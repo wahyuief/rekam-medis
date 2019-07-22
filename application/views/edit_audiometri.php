2a9e33b39f0cba9af78794f2cc6127dab120b7bb
Wahyu Arief <wahyuief@gmail.com>
2019-07-22 11:36:15 +0700

    fix

--- application/views/edit_audiometri.php
+++ application/views/edit_audiometri.php
@@ -7,7 +7,7 @@
 
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Perubahan Data Audiometri Pasien</h6>
+              <h6 class="m-0 font-weight-bold text-primary">Perubahan Data USG Pasien</h6>
             </div>
             <div class="card-body">
               <form method="post">
